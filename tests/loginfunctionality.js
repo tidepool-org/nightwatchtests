adbe31faa0dfd737ef25a677a47723d59c4f6f0f
Ginny Yadav <ginny@tidepool.org>
2020-09-17 17:54:36 -0400

    Code Review Changes - added linter

--- tests/loginfunctionality.js
+++ tests/loginfunctionality.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "Should Login with Correct Credentials"(browser) {
-      const loginPage = browser.page.loginPage();
-      const dsaUsername = browser.globals.dsaUsername
-      const dsaPassword = browser.globals.dsaPassword
+	'Should Login with Correct Credentials'(browser) {
+		const loginPage = browser.page.loginPage();
+		const dsaUsername = browser.globals.dsaUsername;
+		const dsaPassword = browser.globals.dsaPassword;
   
-      loginPage
-        .navigate()
-        .waitForElementVisible("@usernameInput", "Login Page Loaded")
-        .loginDsa(dsaUsername, dsaPassword)
-        .assert.urlContains("data", "Successful Navigation to Patient Data Page")
-        .saveScreenshot("tests_output/login.png");
-    },
-  };
+		loginPage
+			.navigate()
+			.waitForElementVisible('@usernameInput', 'Login Page Loaded')
+			.loginDsa(dsaUsername, dsaPassword)
+			.assert.urlContains('data', 'Successful Navigation to Patient Data Page')
+			.saveScreenshot('tests_output/login.png');
+	},
+};
