aa064c3a030e1b4b66626a041bc366b121e23958
Ginny Yadav <ginny@tidepool.org>
2020-10-03 12:40:44 -0400

    login page ui test

---
+++ tests/ui/loginPageUITests.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  'UI Elements Present'(browser) {
+    const loginPage = browser.page.loginPage();
+    const pageNav = loginPage.section.navigation;
+    const loginForm = loginPage.section.loginForm;
+    const footer = loginPage.section.footer;
+    loginPage.loadPage();
+    pageNav.elementsPresent();
+    loginForm.elementsPresent();
+    footer.elementsPresent();
+  }
+};
