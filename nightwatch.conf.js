9c772e854ef1e16624d0c5d779d91ae2f5611260
Ginny Yadav <ginny@tidepool.org>
2020-10-04 17:16:53 -0400

    add timestamp to display on browserstack dashboard

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,7 +1,7 @@
 'use strict';
 
+var dayjs = require('dayjs');
 require('dotenv').config();
-
 module.exports = {
   src_folders: ['tests'],
   page_objects_path: ['pageobjects'],
@@ -14,43 +14,45 @@ module.exports = {
 
   common_capabilities: {
     'browserstack.user': process.env.BROWSERSTACK_USER,
-    'browserstack.key': process.env.BROWSERSTACK_KEY,
-    name: 'Bstack-[Nightwatch] Parallel Test'
+    'browserstack.key': process.env.BROWSERSTACK_KEY
   },
 
   test_settings: {
     qa2_chrome: {
       launch_url: 'https://qa2.development.tidepool.org',
       desiredCapabilities: {
-        os : "Windows",
-        osVersion : "10",
+        os: 'Windows',
+        osVersion: '10',
         browserName: 'chrome',
         browserVersion: 'latest',
-        resolution: '1366x768'
+        resolution: '1366x768',
+        build: 'QA2_CHROME ' + dayjs().format()
       }
     },
     qa1_chrome: {
       launch_url: 'https://qa1.development.tidepool.org',
       desiredCapabilities: {
-        os : "Windows",
-        osVersion : "10",
+        os: 'Windows',
+        osVersion: '10',
         browserName: 'chrome',
         browserVersion: 'latest',
-        resolution: '1366x768'
+        resolution: '1366x768',
+        build: 'QA1_CHROME ' + dayjs().format()
       }
     },
     prd_chrome: {
       launch_url: 'https://app.tidepool.org',
       desiredCapabilities: {
-        os : "Windows",
-        osVersion : "10",
+        os: 'Windows',
+        osVersion: '10',
         browserName: 'chrome',
         browserVersion: 'latest',
-        resolution: '1366x768'
+        resolution: '1366x768',
+        build: 'PRODUCTION_CHROME ' + dayjs().format()
       }
     }
   },
-    /*
+  /*
     When we're ready to use multi-browser testing we can uncomment these
     - add names to the test script in both the nightwatch.conf.js and travis.yml
 
@@ -70,7 +72,7 @@ module.exports = {
       }
     } */
   globals: {
-    dsaUsername: process.env.DSA_USERNAME,
-    dsaPassword: process.env.DSA_PASSWORD,
+    dsaUsernameTandem: process.env.DSA_USERNAME_TANDEM,
+    dsaPasswordTandem: process.env.DSA_PASSWORD_TANDEM
   }
 };
