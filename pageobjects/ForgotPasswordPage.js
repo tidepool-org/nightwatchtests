e92649113ea449d2a63cbaf2dfc395b2746d1542
Ginny Yadav <ginny@tidepool.org>
2021-01-25 23:46:13 -0500

    create e2e test to ensure 2fa is working

---
+++ pageobjects/ForgotPasswordPage.js
@@ -0,0 +1,79 @@
+const { getToken } = require('../utilities/utilities');
+
+module.exports = {
+  url: function useEnvironmentUrl() {
+    return this.api.launch_url;
+  },
+  sections: {
+    resetForm: {
+      selector: '#app',
+      elements: {
+        logo: 'img[alt="Tidepool"]',
+        forgotPasswordTitle: '.PasswordReset-title',
+        forgotPasswordInstructions: '.PasswordReset-instructions',
+        emailInputLabel: 'label[for="email"]',
+        emailInput: '#email',
+        resetSubmitBtn: {
+          selector: '//button[text()="Send reset link"]',
+          locateStrategy: 'xpath',
+        },
+        loginReturnLink: 'a[href="/login"]',
+      },
+      commands: [{
+      }],
+    },
+    gmail: {
+      selector: 'body',
+      elements: {
+        emailInput: 'input[type="email"]',
+        usernameNextButton: '#identifierNext',
+        passwordNextButton: '#passwordNext',
+        passwordInput: 'input[name="password"]',
+        otpInput: 'input[name="totpPin"]',
+        otpNextButton: '#totpNext',
+        passwordResetEmail: {
+          selector: '(//b[text()="Password reset for your Tidepool account"])',
+          locateStrategy: 'xpath',
+        },
+        emailResetPassButton: 'a[href*="confirm-password-reset"]',
+      },
+      commands: [{
+        accessGmail() {
+          this.api.url('https://mail.google.com');
+          return this.waitForElementPresent('@emailInput')
+            .setValue('@emailInput', this.api.globals.gmailUsername)
+            .click('@usernameNextButton')
+            .waitForElementPresent('@passwordInput')
+            .setValue('@passwordInput', this.api.globals.gmailPassword)
+            .click('@passwordNextButton')
+            .waitForElementPresent('@otpInput')
+            .setValue('@otpInput', getToken())
+            .click('@otpNextButton')
+            .waitForElementPresent('#guser')
+            .click('@passwordResetEmail')
+            .waitForElementPresent('@emailResetPassButton')
+            .click('@emailResetPassButton');
+        },
+      }],
+    },
+    newPasswordForm: {
+      selector: '#app',
+      elements: {
+        changePasswordTitle: '.PasswordReset-title',
+        emailInput: '#email',
+        passwordInput: '#password',
+        passwordConfirmInput: '#passwordConfirm',
+        saveButton: '.simple-form-submit',
+        loginButton: 'a[href="/login"]',
+      },
+      commands: [{
+        switchTab() {
+          return this.api.windowHandles((result) => {
+            const handle = result.value[1];
+            this.switchWindow(handle);
+          });
+        },
+      }],
+    },
+  },
+};
