a7afe23adce7a37a55d4c2f3e4e3605b93cd38cc
Ginny Yadav <ginny@tidepool.org>
2021-01-26 00:13:08 -0500

    travis wants with the old names...

--- tests/e2e/forgotPassword.js
+++ tests/e2e/forgotPassword.js
@@ -2,9 +2,9 @@ const { dsaUsernameTandem, dsaPasswordTandem } = require('../../global');
 
 module.exports = {
   'User Submits Password Reset Form'(browser) {
-    const loginPage = browser.page.LoginPage();
+    const loginPage = browser.page.loginPage();
     const loginForm = loginPage.section.loginForm;
-    const forgotPasswordPage = browser.page.ForgotPasswordPage();
+    const forgotPasswordPage = browser.page.forgotPasswordPage();
     const resetForm = forgotPasswordPage.section.resetForm;
     loginPage.loadPage();
     loginForm.click('@forgotPasswordLink');
@@ -15,12 +15,12 @@ module.exports = {
       .assert.containsText('@forgotPasswordTitle', 'Email sent!', 'Email Sent');
   },
   'User Accesses Email'(browser) {
-    const forgotPasswordPage = browser.page.ForgotPasswordPage();
+    const forgotPasswordPage = browser.page.forgotPasswordPage();
     const gmail = forgotPasswordPage.section.gmail;
     gmail.accessGmail();
   },
   'User Resets Password'(browser) {
-    const forgotPasswordPage = browser.page.ForgotPasswordPage();
+    const forgotPasswordPage = browser.page.forgotPasswordPage();
     const newPasswordForm = forgotPasswordPage.section.newPasswordForm;
     newPasswordForm.switchTab();
     newPasswordForm
--- tests/e2e/helpWidgetFlow.js
+++ tests/e2e/helpWidgetFlow.js
@@ -1,17 +1,17 @@
 module.exports = {
   'User Accesses Help Widget'(browser) {
-    const loginPage = browser.page.LoginPage();
-    const helpWidget = browser.page.HelpWidget();
+    const loginPage = browser.page.loginPage();
+    const helpWidget = browser.page.helpWidget();
     loginPage.loadPage();
     helpWidget.accessHelpWidget();
   },
   'User Searches for "Login" help'(browser) {
-    const helpWidget = browser.page.HelpWidget();
+    const helpWidget = browser.page.helpWidget();
     helpWidget
       .searchArticles('login', browser);
   },
   'User Contacts Support'(browser) {
-    const helpWidget = browser.page.HelpWidget();
+    const helpWidget = browser.page.helpWidget();
     helpWidget.contactSupport();
   },
 };
